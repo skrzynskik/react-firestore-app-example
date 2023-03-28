63c822b5ef9e9b71ae283e32883de9e66f2e5b6d
skrzynski.k <kacperskrzynski@outlook.com>
2023-03-28 23:06:48 +0200

    initial commit + initial changes

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+export default function Main() {
+    return (
+        <>
+        <div className="text-center container mt-3" id="navbar">
+            <h1>firestore-db-app-example</h1>
+            <h3>Authors:</h3>
+            <h4>Kacper Skrzyński · Maciej Choma · Szymon Wieczorek</h4>
+            <hr></hr>
+        </div>
+        </>
+    );
+}
